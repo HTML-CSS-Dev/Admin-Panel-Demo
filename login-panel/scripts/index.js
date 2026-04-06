841445d13be55e5b8c075b9eed6f9ca01013cfb9
shoxzod primov <ranaldu456@gmail.com>
2026-04-06 22:28:55 +0500

    added: login panel

---
+++ login-panel/scripts/index.js
@@ -0,0 +1,29 @@
+const submit = document.forms["send"];
+const fullName = submit['fname'];
+const password = submit['pass'];
+
+
+submit.addEventListener( "submit" , (e) => {
+    e.preventDefault();
+
+    const accounts = JSON.parse(localStorage.getItem("userAccounts")) || [];
+
+    for( let user of accounts ) {
+        if(!(user.name === fullName.value) && !(user.password === password.value)) {
+            alert("The account does not exist")
+            clearingInput();
+            return
+        }
+    }
+
+
+    window.location.href = "http://127.0.0.1:5500/Admin panel/index.html?";
+    clearingInput()
+});
+
+
+// input cleaner
+function clearingInput () {
+    form['fname'].value = null;
+    form['pass'].value = null;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./login-panel/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
